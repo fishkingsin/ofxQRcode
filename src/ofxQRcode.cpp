6981b8775d68cdfcac750445246285b7c15376f1
James Kong <james.kong@nixplay.com>
2017-05-18 14:02:12 +0800

    Update add-ons , add example

--- src/ofxQRcode.cpp
+++ src/ofxQRcode.cpp
@@ -5,12 +5,15 @@ ofxQRcode::fetch(const std::string& data, size_t size, size_t margin)
 {
   std::stringstream googleChartsQRurl;
   googleChartsQRurl
-  << "http://chart.googleapis.com/chart?"   // Google Charts Endpoint
-  << "chs=" << size << "x" << size << "&"   // chart size
-  << "cht=qr&"                              // chart type: QR Code
-  << "chld=L|" << margin << "&"             // Error correction [L,M,Q,H], margin
-  << "choe=UTF-8&"                          // Encoding
-  << "chl=" << data;                        // QR code data
+//  << "http://chart.googleapis.com/chart?"   // Google Charts Endpoint
+//  << "chs=" << size << "x" << size << "&"   // chart size
+//  << "cht=qr&"                              // chart type: QR Code
+//  << "chld=L|" << margin << "&"             // Error correction [L,M,Q,H], margin
+//  << "choe=UTF-8&"                          // Encoding
+//  << "chl=" << data;                        // QR code data
+    << "https://api.qrserver.com/v1/create-qr-code/?"
+    << "size="<<size<<"x"<<size
+    << "&data=" << data;
   
   ofRegisterURLNotification(this);
   ofLoadURLAsync(googleChartsQRurl.str(), "qrcode");
